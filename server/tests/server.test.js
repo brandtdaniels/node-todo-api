13c081f633bcc62724ab5823a48d9ee2028d2e81
Brandt Daniels <brandtdaniels@gmail.com>
2018-09-10 15:40:42 -0700

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }]
 
 beforeEach((done) => {
@@ -194,6 +196,48 @@ describe('An API server', () => {
         request(app)
           .get(`/todos/${hexId}`)
           .expect(404)
+          .end(done)
+
+      })
+
+    })
+
+  })
+
+  context('when updating a todo', () => {
+
+    it('should update the todo', (done) => {
+
+      var hexId = todos[0]._id.toHexString()
+
+      request(app)
+        .patch(`/todos/${hexId}`)
+        .send({text: 'text', completed: true})
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.todo.text).toBe('text').toBeA('string')
+          expect(response.body.todo.completed).toBe(true)
+          expect(response.body.todo.completedAt).toBeA('number')
+        })
+        .end(done)
+
+    })
+
+    context('when todo is not completed', () => {
+
+      it('should clear the completedAt date', (done) => {
+
+        var hexId = todos[1]._id.toHexString()
+
+        request(app)
+          .patch(`/todos/${hexId}`)
+          .send({text: 'text', completed: false})
+          .expect(200)
+          .expect((response) => {
+            expect(response.body.todo.text).toBe('text').toBeA('string')
+            expect(response.body.todo.completed).toBe(false)
+            expect(response.body.todo.completedAt).toNotExist()
+          })
           .end(done)
 
       })
