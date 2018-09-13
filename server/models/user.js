8682f69fb2bac74b77b6d924bd69cd9a4252efb0
Brandt Daniels <brandtdaniels@gmail.com>
2018-09-13 14:38:36 -0700

    Update config to use json

--- server/models/user.js
+++ server/models/user.js
@@ -49,7 +49,7 @@ UserSchema.methods.generateAuthToken = function () {
 
   var access = 'auth'
 
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString()
 
   user.tokens = user.tokens.concat([{access, token}])
 
@@ -81,7 +81,7 @@ UserSchema.statics.findByToken = function (token) {
 
   try {
 
-    decoded = jwt.verify(token, 'abc123')
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
 
   } catch (error) {
 
